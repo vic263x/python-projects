70d5777d155fbbca53c98ff0c935679f6dbc59dd
Wiktoria Połetek <61121658+vic263x@users.noreply.github.com>
2020-04-01 17:00:10 +0200

    implemented fetching the whole document

--- webcrawlers.py
+++ webcrawlers.py
@@ -5,3 +5,27 @@ Created on Wed Apr  1 16:50:14 2020
 @author: Wiktoria
 """
 
+import urllib.request
+from bs4 import BeautifulSoup
+
+print('Getting staff urls...')
+
+staff_url = 'http://wa.amu.edu.pl/wa/en/staff_list'
+response = urllib.request.urlopen(staff_url)
+data = response.read()
+doc = BeautifulSoup(data, 'html.parser')
+
+print(doc)
+
+
+
+
+
+
+
+
+
+
+
+
+
