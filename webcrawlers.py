6880525c229498c770e4d4fa7eb7074f14311ed0
Wiktoria Połetek <61121658+vic263x@users.noreply.github.com>
2020-04-01 17:14:49 +0200

    getting urls

--- webcrawlers.py
+++ webcrawlers.py
@@ -19,8 +19,15 @@ staff_content = doc.find(id='tresc_wlasciwa')
 
 links = staff_content.find_all('a')
 
-print(links)
+urls = []
 
+for link in links:
+    if len(link.get_text()) > 1:
+        url = link['href']
+        urls.append(url)
+        
+print('Urls found:')
+print('\n'.join(urls))
 
 
 
