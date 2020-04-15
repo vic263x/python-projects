2e806a85ed99d0799492871a8fc5e8d54d3ad552
Wiktoria Połetek <61121658+vic263x@users.noreply.github.com>
2020-04-15 13:54:04 +0200

    translation

--- translate.py
+++ translate.py
@@ -5,3 +5,28 @@ Created on Wed Apr 15 13:35:03 2020
 @author: Wiktoria
 """
 
+from googletrans import Translator
+
+
+def main():
+    translator = Translator()
+    result = translator.translate('How are you?', dest= 'es')
+    print(result.text)
+    
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
