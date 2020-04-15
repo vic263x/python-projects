d0c2e5784bcd9c84d96aec4056c2d1e2470c220f
Wiktoria Połetek <61121658+vic263x@users.noreply.github.com>
2020-04-15 14:11:16 +0200

    piratetrans

--- translate.py
+++ translate.py
@@ -6,16 +6,20 @@ Created on Wed Apr 15 13:35:03 2020
 """
 
 from googletrans import Translator
+import requests
 
-
-def main():
+def googletrans():
     translator = Translator()
     result = translator.translate('How are you?', dest= 'es')
     print(result.text)
     
-main()
-
-
+def piratetrans(text):
+    url = 'https://api.funtranslations.com/translate/pirate.json'
+    data = {'text': text}
+    response = requests.post(url, data=data)
+    print(response.text)
+    
+piratetrans('hello sir')
 
 
 
