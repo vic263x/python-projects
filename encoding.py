cfec190a56e71578253b7d317eb583a3c86cafc8
Wiktoria Połetek <61121658+vic263x@users.noreply.github.com>
2020-04-22 16:43:36 +0200

    Russian and Chinese

---
+++ encoding.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 22 13:33:05 2020
+
+@author: Wiktoria
+"""
+
+f = open('russian.txt','rb')
+r = f.read()
+f.close()
+print('Some Russian:', r)
+
+
+f = open('chinese.txt','rb')
+c = f.read()
+f.close()
+print('Some Chinese:', c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
