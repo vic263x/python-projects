da7982713fbb48eb13441aaece898f3d72af0e34
Wiktoria Połetek <61121658+vic263x@users.noreply.github.com>
2020-04-22 16:49:16 +0200

    decoding text from raw bytes

--- encoding.py
+++ encoding.py
@@ -5,14 +5,16 @@ Created on Wed Apr 22 13:33:05 2020
 @author: Wiktoria
 """
 
-f = open('russian.txt','rb')
+f = open('r1251.txt','rb')
 r = f.read()
+r = r.decode('1251') # windows - 1251 encoding
 f.close()
 print('Some Russian:', r)
 
 
-f = open('chinese.txt','rb')
+f = open('cb5.txt','rb')
 c = f.read()
+c = c.decode('big5') # big5 encoding
 f.close()
 print('Some Chinese:', c)
 
