8bcf83756cf7a7539c00ba58aa2f8c11928729d9
Wiktoria Połetek <61121658+vic263x@users.noreply.github.com>
2020-05-06 15:35:48 +0200

    class methods and variables

--- OOP.py
+++ OOP.py
@@ -5,3 +5,20 @@ Created on Wed May  6 15:06:02 2020
 @author: Wiktoria
 """
 
+#a class definition
+class Word: 
+    def whatami():
+        return "I'm a word"
+
+# an instance
+w = Word()
+w2 = Word()
+
+print(Word.whatami())
+
+print(w.whatami())
+
+
+
+
+
