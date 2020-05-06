4f284c38af2b39376dde38bd045ae6c45b6d8d97
Wiktoria Połetek <61121658+vic263x@users.noreply.github.com>
2020-05-06 15:48:15 +0200

    more instance methods

--- OOP.py
+++ OOP.py
@@ -7,18 +7,29 @@ Created on Wed May  6 15:06:02 2020
 
 #a class definition
 class Word: 
+    
+    count = 0
+    
     def whatami(self):
         self.x = "I'm a noun"
 
+    def setDefinition(self, x):
+        self.definition = x
+        Word.count += 1
+        
+    def getDefinitionUppercased(self):
+        return self.definition.upper()
 # an instance
 w = Word()
 w2 = Word()
 
-w.whatami()
-print(w.x)
+w.setDefinition("An animal with four legs")
+print(w.definition)
 
-w2.whatami()
-print(w2.x)
+w2.setDefinition("A vehicle that flies")
+print(w2.definition)
+
+print(w.getDefinitionUppercased())
 
 
 
