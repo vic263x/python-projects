447b191cdf9e91ec704f4e26d11db9bcb323dbe6
Wiktoria Połetek <61121658+vic263x@users.noreply.github.com>
2020-05-06 16:03:51 +0200

    submition

--- OOP.py
+++ OOP.py
@@ -5,31 +5,34 @@ Created on Wed May  6 15:06:02 2020
 @author: Wiktoria
 """
 
-#a class definition
-class Word: 
-    
-    count = 0
-    
-    def whatami(self):
-        self.x = "I'm a noun"
 
-    def setDefinition(self, x):
-        self.definition = x
-        Word.count += 1
+class Phrase: 
+    
+    
+    
+    def sentence(self):
+        self.a = "A new sentence"
+        
+    def setDefinition(self, a):
+        self.definition = a
+        
         
     def getDefinitionUppercased(self):
         return self.definition.upper()
-# an instance
-w = Word()
-w2 = Word()
+    
+p1 = Phrase()
+p2 = Phrase()
 
-w.setDefinition("An animal with four legs")
-print(w.definition)
+p1.setDefinition("A different sentence")
+print(p1.definition)
 
-w2.setDefinition("A vehicle that flies")
-print(w2.definition)
+print(p1.getDefinitionUppercased())
 
-print(w.getDefinitionUppercased())
+p2.setDefinition("Sentence number two")
+print(p2.definition)
+
+print(p2.getDefinitionUppercased())
+    
 
 
 
