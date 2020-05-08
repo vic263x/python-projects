b3dacd71a3337b70288beae4dd16de3936a864b7
Wiktoria Połetek <61121658+vic263x@users.noreply.github.com>
2020-05-08 13:22:28 +0200

    textvariable property

--- GUI.py
+++ GUI.py
@@ -5,16 +5,18 @@ Created on Fri May  8 12:24:23 2020
 @author: Wiktoria
 """
 
-from tkinter import Tk, Button, Label, filedialog, mainloop 
+from tkinter import Tk, Button, Label, filedialog, mainloop, StringVar 
 
 root = Tk()
+file_path = StringVar(root)
+file_path.set("File location...")
 
-file_label = Label(root, text = "File location...")
+file_label = Label(root, textvariable = file_path)
 file_label.pack()
 
 def get_file():
-    file_path = filedialog.askopenfilename()
-    file_label.configure(text = file_path)
+      file_path.set(filedialog.askopenfilename())
+   
 
 file_button = Button(root, text = "get file location", command = get_file)
 file_button.pack()
