4bb713a1ea67bfaa38f192ff80106b3401dcf2c7
Wiktoria Połetek <61121658+vic263x@users.noreply.github.com>
2020-06-10 15:01:27 +0200

    menu widgets

--- more widgets.py
+++ more widgets.py
@@ -5,23 +5,37 @@ Created on Wed Jun 10 13:30:13 2020
 @author: Wiktoria
 """
 
-from tkinter import Tk, Entry, StringVar, Button, mainloop
+from tkinter import Tk, Menu, mainloop
 
-def printit():
-    print('Entry:', text.get())
+
+def newfile():
+    pass
+
+def openfile():
+    pass
+
+def savefile():
+    pass
+
+def closefile():
+    pass
 
 root = Tk()
+menubar = Menu(root)
+filemenu = Menu(menubar)
+filemenu.add_command(label = 'New', command = newfile)
+filemenu.add_command(label = 'Open', command = openfile)
+filemenu.add_command(label = 'Save as...', command = savefile)
+filemenu.add_command(label = 'Close', command = closefile)
+filemenu.add_separator
+filemenu.add_command(label = 'Exit', command = root.quit)
 
-text = StringVar(root)
-text.set('Type something here')
+
+menubar.add_cascade(label = 'File', menu = filemenu)
+
+root.config(menu = menubar)
 
 
-entry = Entry(root, textvariable = text)
-entry.bind('<Return>', lambda x: printit())
-entry.pack()
 
-button = Button(text = 'Print it!', command = printit)
-button.pack()
 mainloop()
-
 
