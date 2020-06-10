658ba96b07a7487a7ae52249ec89af9802a48b89
Wiktoria Połetek <61121658+vic263x@users.noreply.github.com>
2020-06-10 19:00:40 +0200

    scrollbar widget

--- more widgets.py
+++ more widgets.py
@@ -5,21 +5,28 @@ Created on Wed Jun 10 13:30:13 2020
 @author: Wiktoria
 """
 
-from tkinter import Tk, mainloop, Listbox, Button, END, MULTIPLE
+from tkinter import Tk, mainloop, Listbox, Button, END, MULTIPLE, Scrollbar, RIGHT, Y, LEFT, BOTH, BOTTOM
 
 def getitem():
     print('Element(s) selected: ', mylist.curselection())
 
 root = Tk()
 
-mylist = Listbox(root, selectmode = MULTIPLE)
+scrollbar = Scrollbar(root)
+scrollbar.pack(side = RIGHT, fill = Y)
 
-for line in range(5):
+mylist = Listbox(root, selectmode = MULTIPLE, yscrollcommand = scrollbar.set)
+
+scrollbar.config(command = mylist.yview)
+
+for line in range(100):
     mylist.insert(END, 'This is line number ' + str(line))
-mylist.pack()
+mylist.pack(side = LEFT, fill = BOTH)
+
+
 
 button = Button(text = 'Confirm', command = getitem)
-button.pack()
+button.pack(side = BOTTOM)
 
 
 mainloop()
