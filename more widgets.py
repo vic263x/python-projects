ced99de903ab2afa451da96ad7376e5426f1905b
Wiktoria Połetek <61121658+vic263x@users.noreply.github.com>
2020-06-10 13:54:56 +0200

    entry widget

--- more widgets.py
+++ more widgets.py
@@ -5,14 +5,23 @@ Created on Wed Jun 10 13:30:13 2020
 @author: Wiktoria
 """
 
-from tkinter import Tk, messagebox
+from tkinter import Tk, Entry, StringVar, Button, mainloop
 
+def printit():
+    print('Entry:', text.get())
 
 root = Tk()
 
-root.withdraw()
-messagebox.showinfo('Info', 'You are informed!')
+text = StringVar(root)
+text.set('Type something here')
 
-root.deiconify()
 
-root.destroy()
+entry = Entry(root, textvariable = text)
+entry.bind('<Return>', lambda x: printit())
+entry.pack()
+
+button = Button(text = 'Print it!', command = printit)
+button.pack()
+mainloop()
+
+
